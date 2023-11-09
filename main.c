948ba0f80f5638683414581152a75d4e943f951e
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2023-11-09 11:42:30 +0100

    Update quiz game logic and score calculation
    Refactored the quiz game to include error checking, maximum attempts per question, and a refined score calculation system. The scoring system now uses double to allow for half points after the first failed attempt. An exit confirmation prompt was also added to warn the user that all progress will be lost. Introduced a change to terminate the program with a final score and total attempts displayed, if all questions are answered or upon user's exit request.

--- main.c
+++ main.c
@@ -5,8 +5,11 @@
 #include "questions.h"
 
 static char userChoice;
-static int score = 0;
+static double score = 0; // Changed score to double
 static size_t currentQuestionIndex = 0;
+static int attemptCount = 0;
+static int totalAttempts = 0;
+static const int maxAttempts = 2;
 
 void ask_question() {
     const QuizQuestion* currentQuestion = &quizQuestions[currentQuestionIndex];
@@ -14,37 +17,58 @@ void ask_question() {
     printf("%s\n", currentQuestion->question);
     printf("=========================\n"
            "%s\n\n"
-           "Q or q to exit the program\n"
+           "Q or q: to exit the program\n"
            "=========================\n", currentQuestion->choices);
+    attemptCount = 0; // Reset attempt count for each question
 
     printf("Enter your choice: ");
-    scanf(" %c", &userChoice); // add space to ignore leading whitespaces
+    if (scanf(" %c", &userChoice) != 1) { // Error-checking for scanf
+        printf("Invalid input. Please try again.\n");
+        return;
+    }
 
-    switch (toupper(userChoice)) {  // Using toupper() function to avoid case sensitiveness
+    switch (toupper(userChoice)) {
         case 'Q':
             printf("Are you sure you want to exit the program? All progress will be lost. Press y to confirm or any other key to continue: ");
             char confirmExit;
-            scanf(" %c", &confirmExit);
-            if (toupper(confirmExit) == 'Y') {  // Using toupper() here too
+            if (scanf(" %c", &confirmExit) != 1) { // Error-checking for scanf
+                printf("Invalid input. Please try again.\n");
+                return;
+            }
+            if (toupper(confirmExit) == 'Y') {
                 printf("Program terminated.\n");
-                printf("Final score: %d\n", score);
+                printf("Final score: %.1f\n", score); // Display score with one decimal place
+                printf("Total attempts: %d\n", totalAttempts);
                 exit(0);
             }
             return;
         default:
             if(toupper(userChoice) == currentQuestion->correctAnswer) {
                 printf("Good answer!\n");
-                score++;
+                if(attemptCount == 0)
+                    score += 2;
+                else
+                    score += 0.5;  // Increment the score by 0.5 if answered correctly after more than one attempt
                 // Move to the next question
                 if(currentQuestionIndex < questionCount - 1) {
                     currentQuestionIndex++;
+                } else { // If all questions have been answered, print final score and terminate
+                    printf("Well done! You've answered all the questions.\n");
+                    printf("Final score: %.1f\n", score); // Display score with one decimal place
+                    printf("Total attempts: %d\n", totalAttempts);
+                    exit(0);
                 }
             } else {
                 printf("Incorrect answer.\n");
+                if(attemptCount >= maxAttempts - 1) { // If max attempts reached, move to the next question
+                    currentQuestionIndex++;
+                }
             }
+            break;
     }
-    // Print current score
-    printf("Current score: %d\n=========================\n", score);
+    totalAttempts += attemptCount; // Increment total attempts
+    attemptCount++; // Increment attempt count for the current question
+    printf("Current score: %.1f\n=========================\n", score); // Display score with one decimal place
 }
 
 int main() {
@@ -52,6 +76,12 @@ int main() {
     printf("=========================\n");
 
     while (true) {
+        if (currentQuestionIndex >= questionCount) {
+            printf("Well done! You've answered all the questions.\n");
+            printf("Final score: %.1f\n", score); // Display score with one decimal place
+            printf("Total attempts: %d\n", totalAttempts);
+            exit(0);
+        }
         ask_question();
     }
 
